7059dcce2d4d596d40009c0e72f2fe56ad13bff3
yorelandlulu <yoreland@163.com>
2014-09-02 08:25:17 +0800

    re structure

---
+++ src/main/java/com/kun/flow/service/ILogsService.java
@@ -0,0 +1,24 @@
+/**
+ * Program  : IOperaterBindRoleService.java
+ * Author   : songkun
+ * Create   : 2014年4月25日 下午9:22:23
+ *
+ */
+
+package com.kun.flow.service;
+
+import com.kun.flow.exception.ServiceException;
+import com.kun.flow.model.NewsCategory;
+import com.kun.flow.model.WebLogs;
+
+import java.util.List;
+
+/**
+ *
+ * @author songkun
+ * @version 1.0.0
+ * @2014年4月25日 下午9:22:23
+ */
+public interface ILogsService extends IService<WebLogs> {
+
+}
---
+++ src/main/java/com/kun/flow/service/impl/LogsServiceImpl.java
@@ -0,0 +1,14 @@
+package com.kun.flow.service.impl;
+
+import com.kun.flow.model.WebLogs;
+import com.kun.flow.service.ILogsService;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xianing
+ * Date: 7/17/14
+ * Time: 3:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LogsServiceImpl extends AbstractServiceImpl<WebLogs> implements ILogsService {
+}
